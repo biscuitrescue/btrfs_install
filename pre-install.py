10cfe7408262b06c3cc8fd572db2e768fd4e8073
rayquaza-69 <advaymakhija@gmail.com>
2022-04-02 16:09:09 +0530

    .

--- pre-install.py
+++ pre-install.py
@@ -2,3 +2,15 @@
 
 from subprocess import run
 from os.path import exists
+
+disk=input('Disk name: ')
+
+resp=input('''
+This program will completely wipe the disk.
+Are you sure you want to proceed?
+Type 'yes' in all capital letters to proceed: ''')
+
+if resp!='YES':
+    print('Cancelling...')
+    exit()
+
