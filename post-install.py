1ae75f8ae5d399676fb4727fe8d74c64dba92b5e
biscuitrescue <karttikeyasinha11@gmail.com>
2022-04-02 16:40:41 +0530

    post-install configs done

--- post-install.py
+++ post-install.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
-from subprocess import run
 from os.path import exists
+from os import mkdir
+from shutil import copyfile
+
+for i in condict:
+    if not exists(condict[i]):
+        mkdir(condict[i])
+
+for i in condict:
+    file_orig=f"configs/{i}"
+    file_copy=f"{condict[i]}/{i}"
+    copyfile(file_orig, file_copy)
