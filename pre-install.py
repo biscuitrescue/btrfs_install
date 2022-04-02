eac58227dbf2602f7f1cc6697cd8b47090f02365
rayquaza-69 <advaymakhija@gmail.com>
2022-04-02 16:56:16 +0530

    hehe

--- pre-install.py
+++ pre-install.py
@@ -3,7 +3,23 @@
 from subprocess import run
 from os.path import exists
 
-disk=input('Disk name: ')
+def disk_check(s):
+    p1 = run("lsblk | grep "+s+" -w | awk '{print $1}'",shell=True,capture_output=True,text=True)
+    if p1.stdout.strip() !='':
+        return True
+    else:
+        return False
+
+while True: 
+    disk=input('Disk name [/dev/sdX]: ')
+    if disk_check(disk[5:]):
+        print('Disk exists.')
+        print()
+        break
+    else:
+        print('Disk does not exist.')
+        print()
+        continue
 
 resp=input('''
 This program will completely wipe the disk.
@@ -13,4 +29,9 @@ Type 'yes' in all capital letters to proceed: ''')
 if resp!='YES':
     print('Cancelling...')
     exit()
+
+print('Creating new partition table...')
+run('parted '+disk+' mklabel gpt',shell=True)
+print('Created new GPT partition table.')
+print()
 
